54c5b90cd13f5a8f882dc3ce405965407486f792
Chuanyang10m <51302672+Chuanyang10m@users.noreply.github.com>
2019-06-10 16:58:40 +1000

    Add files via upload
    This is the code that sets up a range of animations for characters, Warrior, Rogue Mage.

---
+++ CharacterAnimations.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is used to connect script and condition parameters in Animator Controller 
+/// Attach this code to characters
+/// </summary>
+
+
+public class CharacterAnimations : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        // Initialise the Animator component for anim;
+        anim = GetComponent<Animator>();
+    }
+
+    // Set the condition value for Walk animation, 
+    // so Walk animation plays
+    public void Walk(bool walk)
+    {
+        anim.SetBool("Walk", walk);
+    }
+
+    #region Animations for Warrior .................
+
+    //set the condition value for Warrior attack
+    public void Attack_Warrior()
+    {
+        anim.SetTrigger("Attack Warrior");
+    }
+
+    //set the condition value for Warrior DEFEND
+    public void Defend(bool defend)
+    {
+        anim.SetBool("Defend", defend);
+    }
+
+    #endregion
+
+    #region Animations for Rogue ...................
+
+    //set the condition value for Rogue attack
+    public void Attack_Rogue()
+    {
+        anim.SetTrigger("Attack Rogue");
+    }
+
+
+    public void Attack_Shoot()
+    {
+        anim.SetTrigger("Attack Shoot");
+    }
+
+    #endregion
+
+    #region Animations for Mage ....................
+
+    //set the condition value for Mage Attack
+    public void Attack_Mage()
+    {
+        anim.SetTrigger("Attack Mage");
+    }
+
+    public void Cast_Mage()
+    {
+        anim.SetTrigger("Cast Mage");
+    }
+
+    #endregion
+
+
+
+                 
+
+}
