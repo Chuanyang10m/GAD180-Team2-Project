00e3be0178897b4bff5f88b78c42a814906db7e3
Zack Donaldson <51683489+Zekk98-design@users.noreply.github.com>
2019-06-21 11:03:03 +1000

    My old scripts that im sure can be altered for this project

---
+++ DeactivateOnCollision3D.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// © 2017 TheFlyingKeyboard and released under MIT License
+// theflyingkeyboard.net
+//Deactivates given GameObject on collision
+public class DeactivateOnCollision3D : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToDeactivate;
+    [SerializeField] private string objectTagName;
+    [SerializeField] private string objectName;
+    [SerializeField] private bool deactivateThisObject = false;
+    private void Start()
+    {
+        if (deactivateThisObject)
+        {
+            objectToDeactivate = gameObject;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(objectTagName) || collision.gameObject.name == objectName)
+        {
+            objectToDeactivate.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag(objectTagName) || collider.gameObject.name == objectName)
+        {
+            objectToDeactivate.SetActive(false);
+        }
+    }
+}
---
+++ ObjectCollection.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectCollection : MonoBehaviour
+{
+
+    public Text countText;
+    public Text winText;
+
+    [SerializeField] private int count;
+
+    void OnCollisionEnter(Collision Col)
+    {
+        if (Col.gameObject.name == "Axe")
+        {
+            count = count + 1;
+            SetCountText();
+            Destroy(Col.gameObject);
+        }
+        if (Col.gameObject.name == "Bread")
+        {
+            count = count + 1;
+            SetCountText();
+            Destroy(Col.gameObject);
+        }
+        if (Col.gameObject.name == "Apple")
+        {
+            count = count + 1;
+            SetCountText();
+            Destroy(Col.gameObject);
+        }
+        if (Col.gameObject.name == "Bull Skull (XP Loss)")
+        {
+            count = count + 1;
+            SetCountText();
+            Destroy(Col.gameObject);
+        }
+    } 
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 4)
+        {
+            winText.text = "win";
+        }
+    }
+
+        // Start is called before the first frame update
+            void Start()
+    {
+        winText.text = "You win!";
+        SetCountText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
